24572bc5fb9774273b372114891fa785a507d851
rnicar245 <rj.nieto.car@gmail.com>
2020-01-10 13:17:55 +0100

    Añadidos getters close #5

--- Empleado.js
+++ Empleado.js
@@ -13,12 +13,24 @@
         let ventana = window.open("", "", "top=0,left=0,width=1000,height=1000");
         ventana.document.write("<html><head></head><body>"
         + "<h2>Nombre:</h2>"
-        + "<h3>"+this.nombre+"</h3><br>"
+        + "<h3>"+this.getNombre()+"</h3><br>"
         + "<h2>Fecha de nacimiento:</h2>"
-        + "<h3>"+this.fecha+"</h3><br>"
+        + "<h3>"+this.getFecha()+"</h3><br>"
         + "<h2>DNI:</h2>"
-        + "<h3>"+this.dni+"</h3><br>"
+        + "<h3>"+this.getDni()+"</h3><br>"
         + "</body></html>");
         ventana.document.close();
+    }
+
+    Empleado.prototype.getNombre = function(){
+        return this.nombre;
+    }
+
+    Empleado.prototype.getFecha = function(){
+        return this.fecha;
+    }
+
+    Empleado.prototype.getDni = function(){
+        return this.dni;
     }
 }
